7a62d52596748e7f39b525bd48538c07ef54914b
Dmitry029 <kdp.029029@cmail.com>
2025-05-31 21:42:25 +0300

    use db for keet auth data

--- src/main/java/com/luv2code/springboot/cruddemo/security/DemoSecurityConfig.java
+++ src/main/java/com/luv2code/springboot/cruddemo/security/DemoSecurityConfig.java
@@ -3,19 +3,27 @@ package com.luv2code.springboot.cruddemo.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.authentication.jaas.memory.InMemoryConfiguration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class DemoSecurityConfig {
 
+    // add support for JDBC ... no more hardcoded users
+
     @Bean
+    public UserDetailsManager userDetailsManager(DataSource dataSource) {
+        return new JdbcUserDetailsManager(dataSource);
+    }
+
+
+ /*   @Bean
     public InMemoryUserDetailsManager userDetailsManager() {
 
         UserDetails john = User.builder()
@@ -36,7 +44,7 @@ public class DemoSecurityConfig {
                 .roles("EMPLOYEE", "MANAGER", "ADMIN")
                 .build();
         return new InMemoryUserDetailsManager(john, mary, susan);
-    }
+    }*/
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
